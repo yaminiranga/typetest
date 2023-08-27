26665c33d0e8222d07052f91abf42f3014afd8ec
yyamini <yaministarlight@gmail.com>
2023-08-27 17:03:47 +0530

    toggle logic corrected

--- src/components/StartButton.js
+++ src/components/StartButton.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const StartButton = (props) => {
     const toggleButton = props.toggle
-    const isButton = props.startbtn
+    const isButton = props.isStartBtn
     return (
         <>
             <button onClick={toggleButton}>{isButton ? 'Stop' : 'Start'}</button>
